7fa540a2d2fc6214f74be3a5c6cf48f1310b7d93
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-06-19 19:07:18 -0400

    link-mapa

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function initMap(){
+	var map = new google.maps.Map(document.getElementById("map"),{
+		zoom: 5,
+		center: {lat: -9.1191427, lng: -77.0349046},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	});
+
+}
